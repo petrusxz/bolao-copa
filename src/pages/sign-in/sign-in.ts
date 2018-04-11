d38570e3169930a57e9107a939031736139a632a
Petrus Cyrino <petrusxz@gmail.com>
2018-04-11 20:39:28 -0300

    Auth Pages

---
+++ src/pages/sign-in/sign-in.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-sign-in',
+  templateUrl: 'sign-in.html'
+})
+export class SignInPage {
+  constructor() {}
+}
---
+++ src/pages/sign-up/sign-up.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { IonicPage } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-sign-up',
+  templateUrl: 'sign-up.html'
+})
+export class SignUpPage {
+  constructor() {}
+}
